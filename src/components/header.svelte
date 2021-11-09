8a5c67c5025dca41665b3eface4c1c7f770bc2b6
PrasanaSmokz <62689892+PrasanaSmokz@users.noreply.github.com>
2021-11-09 18:53:51 +0530

    done

---
+++ src/components/header.svelte
@@ -0,0 +1,11 @@
+<script>
+    import ProductCategorySlider from "./product-categorySlider.svelte";
+    import TopBar from "./top-bar.svelte";
+</script>
+<div class=" bg-customBrown-200">
+    <TopBar/>
+    <ProductCategorySlider/>
+</div>
+<style>
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,12 @@
 <script>
 	import ProductList from "../components/product-list.svelte";
+	import Header from "../components/header.svelte";
+	import CategorySlider from "../components/category-slider.svelte";
 </script>
 <div>
+	<Header/>
+	<CategorySlider/>
 	<ProductList/>
 </div>
 <style>
-
 </style>
